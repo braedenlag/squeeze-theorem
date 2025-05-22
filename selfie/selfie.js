211727b145affaac785d2abd07ded3dde1d18d33
braedenlag <braedenlag@gmail.com>
2025-05-21 21:00:49 -0400

    starting selfie gif, crops to circle

--- selfie/selfie.js
+++ selfie/selfie.js
@@ -11,12 +11,32 @@ selfie.getCamera = () => {
     .then((stream) => {
         video.srcObject = stream;
         video.play();
-        ctx.drawImage(video, 0, 0);
-        console.log(canvas.toDataURL("image/png"));
+        video.addEventListener('loadeddata', () => {
+            selfie.drawFrame(video);
+          });
     })
     .catch((err) => {
         console.error(`An error occurred: ${err}`);
     });
+}
+
+selfie.drawFrame = (video) => {
+    if(video.videoWidth > video.videoHeight) {
+        //keep height same, center horizontally
+        const scale = video.videoWidth / video.videoHeight;
+        const offset = (canvas.width - (canvas.height * scale)) / 2;
+        ctx.drawImage(video, offset, 0, canvas.height * scale, canvas.height);
+    } else {
+        //keep width same, center vertically
+        const scale = video.videoHeight / video.videoWidth;
+        const offset = (canvas.height - (canvas.width * scale)) / 2;
+        ctx.drawImage(video, 0, offset, canvas.width, canvas.width * scale);
+    }
+    ctx.globalCompositeOperation='destination-in';
+    ctx.beginPath();
+    ctx.arc(canvas.width/2,canvas.height/2,canvas.height/2,0,Math.PI*2);
+    ctx.closePath();
+    ctx.fill();
 }
 
 window.onload = selfie.startup;
