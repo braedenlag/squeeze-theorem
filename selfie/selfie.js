c60831f5994d4e952044a5e68736d744d0358ed7
braedenlag <braedenlag@gmail.com>
2025-05-24 22:20:13 -0400

    made it loop properly, proper sized canvas for mobile

--- selfie/selfie.js
+++ selfie/selfie.js
@@ -1,19 +1,40 @@
 const selfie = {};
 selfie.startup = () => {
     selfie.video = document.getElementById("cameraView");
+    selfie.result = document.getElementById("selfieResult");
     selfie.canvas = document.getElementById("selfieGIF");
-    selfie.ctx = selfie.canvas.getContext("2d");
+    selfie.ctx = selfie.canvas.getContext("2d", { willReadFrequently: true });
     selfie.overlay = document.getElementById("selfieOverlay");
-    selfie.octx = selfie.overlay.getContext("2d");
+    selfie.octx = selfie.overlay.getContext("2d", { willReadFrequently: true });
     
     selfie.frame = 0;
     selfie.gifLength = 31; //number of frames
+    selfie.delay = 100;
+
+    selfie.gif = new GIF({
+        workers: 10,
+        quality: 1000,
+        width: 450,
+        height: 450,
+        transparent: 'rgba(0, 0, 0, 0)',
+        dither: 'Atkinson',
+        worker: 'gif.worker.js'
+    });
+    selfie.gif.on('finished', function(blob) {
+        selfie.result.src = (URL.createObjectURL(blob));
+        selfie.ctx.clearRect(0, 0, selfie.canvas.width, selfie.canvas.height);
+        selfie.octx.clearRect(0, 0, selfie.overlay.width, selfie.overlay.height);
+        //mark render as finished so it can properly run again
+        selfie.gif.running = false;
+        selfie.gif.frames = [];
+    });
 
     selfie.recording = false;
     document.getElementById("startSelfie").onclick = selfie.getCamera;
 }
 
 selfie.getCamera = () => {
+    
     if(selfie.recording) return;
     navigator.mediaDevices
     .getUserMedia({ video: true, audio: false })
@@ -28,26 +49,25 @@ selfie.getCamera = () => {
     });
 }
 
+selfie.removeCamera = () => {
+    selfie.video.srcObject.getTracks().forEach(function(track) {
+        track.stop();
+    });
+}
+
 selfie.startCountdown = () => {
-    selfie.gif = new GIF({
-        workers: 5,
-        quality: 100,
-        worker: 'gif.worker.js'
-    });
-    selfie.gif.on('finished', function(blob) {
-        window.open(URL.createObjectURL(blob));
-        selfie.ctx.clearRect(0, 0, selfie.canvas.width, selfie.canvas.height);
-        selfie.octx.clearRect(0, 0, selfie.overlay.width, selfie.overlay.height);
-    });
     selfie.recording = true;
-    selfie.interval = setInterval(selfie.drawFrame, 100); //10 fps
+    clearInterval(selfie.interval)
+    selfie.interval = setInterval(selfie.drawFrame, selfie.delay);
 }
 
 selfie.drawFrame = () => {
-    console.log(performance.now())
     const video = selfie.video
     const ctx = selfie.ctx;
     const canvas = selfie.canvas;
+
+    selfie.result.src = "";
+
     if(video.videoWidth > video.videoHeight) {
         //keep height same, center horizontally
         const scale = video.videoWidth / video.videoHeight;
@@ -69,13 +89,13 @@ selfie.drawFrame = () => {
     
     if(selfie.recording) {
         selfie.frame++;
-        selfie.gif.addFrame(canvas, {copy: true, delay: 10});
+        selfie.gif.addFrame(canvas, {copy: true, delay: selfie.delay});
         
         //circular progress bar
         const startingAngle = 3 * Math.PI / 2;
         selfie.octx.beginPath();
         selfie.octx.lineWidth = 8;
-        selfie.octx.arc(canvas.width/2, canvas.height/2, canvas.height/2 - 5, startingAngle, startingAngle + Math.PI * 2 / selfie.gifLength * selfie.frame);
+        selfie.octx.arc(canvas.width/2, canvas.height/2, canvas.height/2 - 5, startingAngle, (startingAngle + 0.001) + ((Math.PI * 2) / selfie.gifLength * selfie.frame));
         selfie.octx.strokeStyle = "rgb(240, 97, 97)";
         selfie.octx.stroke();
         selfie.octx.closePath();
@@ -85,9 +105,6 @@ selfie.drawFrame = () => {
             clearInterval(selfie.interval);
             selfie.frame = 0;
             selfie.recording = false;
-            selfie.video.srcObject.getTracks().forEach(function(track) {
-                track.stop();
-            });
         }
     }
 }
