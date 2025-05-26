47399f911b1d6c52b7f8a81006e3f12e93a2591a
braedenlag <braedenlag@gmail.com>
2025-05-26 16:23:54 -0400

    added filters

--- selfie/selfie.js
+++ selfie/selfie.js
@@ -23,22 +23,29 @@ selfie.startup = () => {
         dither: 'Atkinson',
         worker: 'gif.worker.js'
     });
+    
+    selfie.originalGif = {};
+
     selfie.gif.on('finished', function(blob) {
         selfie.result.src = (URL.createObjectURL(blob));
         document.getElementById("downloadLink").href = selfie.result.src;
         document.getElementById("downloadLink").setAttribute("data-disabled", "false");
         document.getElementById("startSelfie").setAttribute("data-disabled", "false");
+        document.getElementById("selfie-filter").disabled = false;
         selfie.ctx.clearRect(0, 0, selfie.canvas.width, selfie.canvas.height);
         selfie.octx.clearRect(0, 0, selfie.overlay.width, selfie.overlay.height);
         selfie.canvas.setAttribute("data-processing", "false");
         document.getElementById("loadingcontainer").style.visibility = "hidden";
-        //mark render as finished so it can properly run again
         selfie.gif.running = false;
-        selfie.gif.frames = [];
     });
 
     selfie.recording = false;
     document.getElementById("startSelfie").onclick = selfie.getCamera;
+
+    document.getElementById("selfie-filter").onchange = () => {
+        if(document.getElementById("selfieResult").src == "") return;
+        selfie.doRender();
+    }
 }
 
 selfie.getCamera = () => {
@@ -66,10 +73,15 @@ selfie.removeCamera = () => {
 
 selfie.startCountdown = () => {
     selfie.timeout = 3000;
+    //mark render as finished so it can properly run again
+    selfie.gif.frames = [];
+    selfie.originalGif.frames = [];
+
     document.getElementById("downloadLink").removeAttribute("href");
     document.getElementById("downloadLink").setAttribute("data-disabled", "true");
     document.getElementById("startSelfie").setAttribute("data-disabled", "true");
     document.getElementById("stopSelfie").setAttribute("data-disabled", "true");
+    document.getElementById("selfie-filter").disabled = true;
     selfie.timeout += selfie.delay;
     selfie.timeoutStart = performance.now();
     selfie.canvas.setAttribute("data-processing", "true");
@@ -77,8 +89,75 @@ selfie.startCountdown = () => {
     selfie.interval = setInterval(selfie.drawFrame, selfie.delay);
 }
 
+selfie.applyFilter = () => {
+    const selectedFilter = document.getElementById("selfie-filter").value;
+    switch (selectedFilter) {
+        case "no-filter":
+            selfie.revert();
+            break;
+        case "grayscale":
+            selfie.grayscale();
+            break;
+        case "night-vision":
+            selfie.nightVision();
+            break;
+        case "invert":
+            selfie.invert();
+            break;
+    }
+}
+
+selfie.revert = () => {
+    for(let i = 0; i < selfie.originalGif.frames.length; i++)
+    {
+        let data = selfie.originalGif.frames[i].data;
+        selfie.gif.frames[i].data = new Uint8ClampedArray(data);;
+    }
+}
+
+selfie.grayscale = () => {
+    for(let i = 0; i < selfie.originalGif.frames.length; i++)
+    {
+        let data = new Uint8ClampedArray(selfie.originalGif.frames[i].data);
+        for (let j = 0; i < data.length; j += 4) {
+            let gray = (data[j] + data[j+1] + data[j+2]) / 3
+            data[j] = gray; // red
+            data[j + 1] = gray; // green
+            data[j + 2] = gray; // blue
+        }
+        selfie.gif.frames[i].data = new Uint8ClampedArray(data);;
+    }
+}
+
+selfie.nightVision = () => {
+    for(let i = 0; i < selfie.originalGif.frames.length; i++)
+    {
+        let data = new Uint8ClampedArray(selfie.originalGif.frames[i].data);
+        for (let j = 0; j < data.length; j += 4) {
+            let gray = ((data[j] + data[j+1] + data[j+2]) / 3) * (Math.random() * (0.7 - 0.5) + 0.5);
+            data[j] = gray * 0.5; // red
+            data[j + 1] = gray * 2; // green
+            data[j + 2] = gray * 0.5; // blue
+        }
+        selfie.gif.frames[i].data = new Uint8ClampedArray(data);;
+    }
+}
+
+selfie.invert = () => {
+    for(let i = 0; i < selfie.originalGif.frames.length; i++)
+    {
+        let data = new Uint8ClampedArray(selfie.originalGif.frames[i].data);
+        for (let j = 0; j < data.length; j += 4) {
+            data[j] = 255 - data[j]; // red
+            data[j + 1] = 255 - data[j + 1]; // green
+            data[j + 2] = 255 - data[j + 2]; // blue
+        }
+        selfie.gif.frames[i].data = new Uint8ClampedArray(data);;
+    }
+}
+
 selfie.drawFrame = () => {
-    const video = selfie.video
+    let video = selfie.video
     const ctx = selfie.ctx;
     const canvas = selfie.canvas;
 
@@ -117,11 +196,13 @@ selfie.drawFrame = () => {
     ctx.closePath();
     ctx.fill();
     ctx.globalCompositeOperation='source-over';
+
     
     if(selfie.recording) {
         selfie.frame++;
         selfie.gif.addFrame(canvas, {copy: true, delay: selfie.delay});
-        
+        const imageData = selfie.ctx.getImageData(0, 0, selfie.canvas.width, selfie.canvas.height);
+        selfie.originalGif.frames.push({ data: new Uint8ClampedArray(imageData.data), delay: selfie.delay });
         //circular progress bar
         const startingAngle = 3 * Math.PI / 2;
         selfie.octx.beginPath();
@@ -132,15 +213,22 @@ selfie.drawFrame = () => {
         selfie.octx.closePath();
 
         if(selfie.frame >= selfie.gifLength) {
-            selfie.canvas.setAttribute("data-processing", "true");
-            document.getElementById("stopSelfie").setAttribute("data-disabled", "false");
-            document.getElementById("loadingcontainer").style.visibility = "visible";
-            selfie.gif.render();
-            clearInterval(selfie.interval);
-            selfie.frame = 0;
-            selfie.recording = false;
+            selfie.doRender();
         }
     }
+}
+
+selfie.doRender = () => {
+    selfie.result.src = "";
+    selfie.canvas.setAttribute("data-processing", "true");
+    document.getElementById("selfie-filter").disabled = true;
+    document.getElementById("stopSelfie").setAttribute("data-disabled", "false");
+    document.getElementById("loadingcontainer").style.visibility = "visible";
+    selfie.applyFilter();
+    selfie.gif.render();
+    clearInterval(selfie.interval);
+    selfie.frame = 0;
+    selfie.recording = false;
 }
 
 window.onload = selfie.startup;
